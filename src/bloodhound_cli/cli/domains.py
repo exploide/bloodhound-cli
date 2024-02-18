d5e4c20f508f7bc31ae78f8dc54dcaaef70d13c4
exploide <me@exploide.net>
2024-02-18 12:19:52 +0100

    domains: sort domains by name

--- src/bloodhound_cli/cli/domains.py
+++ src/bloodhound_cli/cli/domains.py
@@ -5,11 +5,14 @@ from bloodhound_cli.api.from_config import api
 
 @click.command()
 @click.option("--sid", is_flag=True, help="Show SIDs.")
-@click.option("--sep", "-s", metavar="SEP", default="\t", help="Separator between values (default: tab).")
+@click.option("--sep", "-s", metavar="SEP", default="\t", help="Separator between fields (default: tab).")
 def domains(sid, sep):
     """Get info on domains."""
 
-    for domain in api.domains():
+    result = api.domains()
+    result = sorted(result, key=lambda d: d["name"])
+
+    for domain in result:
         out = [domain["name"]]
         if sid:
             out.append(domain["id"])
