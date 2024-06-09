08c3c1734a7e5c3efe35027bfa4fa105b51cb105
exploide <me@exploide.net>
2024-06-09 12:09:03 +0200

    domains: added --collected / --not-collected options
    closes #2

--- src/bloodhound_cli/cli/domains.py
+++ src/bloodhound_cli/cli/domains.py
@@ -4,15 +4,18 @@ from bloodhound_cli.api.from_config import api
 
 
 @click.command()
+@click.option("--collected/--not-collected", default=None, help="Show only (not) collected domains.")
 @click.option("--sid", is_flag=True, help="Show SIDs.")
 @click.option("--sep", "-s", metavar="SEP", default="\t", help="Separator between fields (default: tab).")
-def domains(sid, sep):
+def domains(collected, sid, sep):
     """Get lists of domains."""
 
     result = api.domains()
     result = sorted(result, key=lambda d: d["name"])
 
     for domain in result:
+        if collected is not None and collected != domain.get("collected", False):
+            continue
         out = [domain["name"]]
         if sid:
             out.append(domain["id"])
